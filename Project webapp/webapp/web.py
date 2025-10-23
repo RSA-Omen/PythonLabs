bb509e69650a3a36e8dcea6573f3eb5d7bd1d6ff
Lauchlan <lauchlan@example.com>
2025-10-23 18:25:52 +0200

    added functionality for todos to the webapp

--- Project webapp/webapp/web.py
+++ Project webapp/webapp/web.py
@@ -3,7 +3,16 @@
 """
 
 import streamlit as st
+import functions
+
+todos = functions.get_todos()
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to help you track your todos.")
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label="Enter a todo", placeholder="Add a new todo...")
+
