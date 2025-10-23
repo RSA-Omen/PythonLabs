242a8d18c14b19e15bcef438226573b43150b07c
Lauchlan <lauchlan@example.com>
2025-10-23 18:32:07 +0200

    Fix todo app to properly load and display todos from file

--- Project webapp/webapp/functions.py
+++ Project webapp/webapp/functions.py
@@ -1,14 +1,19 @@
-def get_todos(filepath="/home/lauchlan/thelab/todos.txt"):
+import os
+
+def get_todos(filepath="todos.txt"):
     """ Read a text file and return the list of to-do items."""
     try:
         with open(filepath, "r") as file:
             todos = file.readlines()
+        # Strip newlines and filter out empty lines
+        todos = [todo.strip() for todo in todos if todo.strip()]
         return todos
     except FileNotFoundError:
         print("The todo file does not exist. Please add an item first.")
         return []    
 
-def write_todos(todos_arg, filepath="/home/lauchlan/thelab/todos.txt"):
+def write_todos(todos_arg, filepath="todos.txt"):
     """ Write the to-do items list in the text file."""
     with open(filepath, "w") as file:
-        file.writelines(todos_arg)
+        for todo in todos_arg:
+            file.write(todo + "\n")
--- Project webapp/webapp/web.py
+++ Project webapp/webapp/web.py
@@ -11,8 +11,19 @@ st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to help you track your todos.")
 
-for todo in todos:
-    st.checkbox(todo)
+# Display existing todos
+if todos:
+    st.write("**Your current todos:**")
+    for todo in todos:
+        st.checkbox(todo)
+else:
+    st.write("No todos yet!")
 
-st.text_input(label="Enter a todo", placeholder="Add a new todo...")
+# Add new todo
+st.subheader("Add New Todo")
+new_todo = st.text_input(label="Enter a todo", placeholder="Add a new todo...")
+if st.button("Add Todo") and new_todo:
+    todos.append(new_todo)
+    functions.write_todos(todos)
+    st.rerun()
 
